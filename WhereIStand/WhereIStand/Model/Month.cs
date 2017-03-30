c7a6bef76422b51a51e1d385ecb7dcec16ddc338
daniel verman <daniverman@gmail.com>
2017-03-30 13:51:47 +0300

    d

---
+++ WhereIStand/WhereIStand/Model/Month.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace WhereIStand.Model
+{
+    internal class Month
+    {
+        private Dictionary<string, Day> monthAss;
+
+        public Month()
+        {
+            monthAss = new Dictionary<string, Day>();
+        }
+
+        // public SortedList<> GetAssignmentDay(string day)
+        //{
+        //}
+    }
+}
