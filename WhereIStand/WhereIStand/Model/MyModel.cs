895502bd89db2b7cde94bcfca69784081bd54384
Eliran <elirannach@gmail.com>
2017-03-30 12:11:27 +0300

    gsfd

---
+++ WhereIStand/WhereIStand/Model/MyModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereIStand.Model
+{
+    class MyModel : INotifyPropertyChanged
+    {
+        public MyModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void notifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
---
+++ WhereIStand/WhereIStand/ViewModel/MyViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhereIStand.Model;
+
+namespace WhereIStand.ViewModel
+{
+    class MyViewModel : INotifyPropertyChanged
+    {
+        MyModel model;
+        public MyViewModel(MyModel model)
+        {
+            this.model = model;
+            model.PropertyChanged += (sender, e) =>
+            {
+                notifyPropertyChanged("VM_" + e.PropertyName);
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void notifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
