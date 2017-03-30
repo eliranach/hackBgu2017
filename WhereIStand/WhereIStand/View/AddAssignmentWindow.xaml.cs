0a77942674f017817c9dfbfde9bc1d1ab94ee33c
Eliran <Eliran@ELIRAN>
2017-03-31 02:26:02 +0300

    gjhgfj

---
+++ WhereIStand/WhereIStand/View/AddAssignmentWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WhereIStand.View
+{
+    /// <summary>
+    /// Interaction logic for AddAssignmentWindow.xaml
+    /// </summary>
+    public partial class AddAssignmentWindow : Window
+    {
+        public AddAssignmentWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void saveClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- WhereIStand/WhereIStand/View/Controls/calendar.xaml.cs
+++ WhereIStand/WhereIStand/View/Controls/calendar.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WhereIStand;
 using WhereIStand.Model;
 using WhereIStand.ViewModel;
 
@@ -28,6 +29,7 @@ namespace WhereIStand.View.Controls
         private string VM_chooseDate;
         string m_selectedDate;
         List<assignment> currentAss;
+        private readonly CalenderBackground background;
 
         public string ChooseDate
         {
@@ -40,6 +42,15 @@ namespace WhereIStand.View.Controls
         {
             InitializeComponent();
             m_vm = vm;
+            background = new CalenderBackground(cl);
+            background.AddOverlay("circle", "circle.png");
+            background.AddOverlay("tjek", "tjek.png");
+            background.AddOverlay("cross", "cross.png");
+            background.AddOverlay("box", "box.png");
+            background.AddOverlay("gray", "gray.png");
+            cl.Background = background.GetBackground();
+            // Update background when changing the shown month
+            cl.DisplayDateChanged += KalenderOnDisplayDateChanged;
             currentAss = new List<assignment>();
             this.DataContext = vm;
             m_vm.PropertyChanged += (sender, e) =>
@@ -51,6 +62,16 @@ namespace WhereIStand.View.Controls
                     Console.ReadKey();
                 }
             };
+            fillCalendar();
+        }
+
+        private void fillCalendar()
+        {
+            foreach (assignment assign in m_vm.VM_AssignmentList)
+            {
+                DateTime dt = assign.assignmentDate;
+                background.AddDate(dt, "circle");
+            }
         }
 
         private void updateTask()
@@ -58,11 +79,22 @@ namespace WhereIStand.View.Controls
 
         }
 
+        private void KalenderOnDisplayDateChanged(object sender, CalendarDateChangedEventArgs calendarDateChangedEventArgs)
+        {
+            cl.Background = background.GetBackground();
+        }
+
         private void Calendar_OnSelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
             //string[] date = e.AddedItems[0].ToString().Split('/');
             //DateTime dateTime = new DateTime(2017, Int32.Parse(date[0]), Int32.Parse(date[1]));
             //m_vm.setDayAssinment(dateTime);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddAssignmentWindow assignmentW = new AddAssignmentWindow();
+            assignmentW.Show();
         }
     }
 }
