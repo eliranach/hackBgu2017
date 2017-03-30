dee676db089a6eb06d178d6d1259dd815823c17f
ilanser <sernov@post.bgu.ac.il>
2017-03-30 13:41:22 +0300

    sfgahar

--- WhereIStand/WhereIStand/View/MainWindow.xaml.cs
+++ WhereIStand/WhereIStand/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WhereIStand.Model;
 
 namespace WhereIStand
 {
@@ -23,6 +24,7 @@ namespace WhereIStand
         public MainWindow()
         {
             InitializeComponent();
+            //Connector con = new Connector();
         }
     }
 }
